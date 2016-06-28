75600a20b2a3325de138f9e5f43aef8ab791f640
Justin Ober <justindober@gmail.com>
2016-06-27 21:22:16 -0600

    First Commit

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function setKarmaConfig(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: true,
     concurrency: Infinity
   })
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+const webpack = require('webpack')
 const {resolve} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = env => {
+
+  const addPlugin = (add, plugin) => add ? plugin : undefined
+  const ifProd = plugin => addPlugin(env.prod, plugin)
+  const ifDev = plugin => addPlugin(env.dev, plugin)
+  const ifTest = plugin => addPlugin(env.test, plugin)
+  const ifNotTest = plugin => addPlugin(!env.test, plugin)
+  const removeEmpty = array => array.filter(i => !!i)
+
   return {
-    entry: './js/app.js',
+    entry: {
+      vendor: ['lodash','jquery'],
+      app: './js/app.js'
+    },
     output: {
-      filename: 'bundle.js',
+      filename: 'bundle.[name].[chunkhash].js',
       path: resolve(__dirname, 'dist'),
       pathinfo: !env.prod,
     },
@@ -16,5 +30,32 @@ module.exports = env => {
         {test: /\.css$/, loader: 'style!css'},
       ],
     },
+
+    plugins: removeEmpty([
+
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      ifProd(new webpack.optimize.DedupePlugin()),
+      ifProd(new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+      })),
+      ifProd(new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"',
+        }
+      })),
+      ifProd(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true, //eslint-disable-line
+          warnings: false,
+        }
+      })), 
+      ifNotTest(new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+      }))
+
+    ])
   }
 }
